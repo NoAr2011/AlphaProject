4a8c364474fc091206c49bf2c93fd4b46932e2ca
Bihari Richárd <biharirichard615@gmail.com>
2025-05-13 08:43:21 +0200

    Bug fixes in the conenction file.

--- DataBaseConnection.fs
+++ DataBaseConnection.fs
@@ -4,9 +4,7 @@ open System.Data.SQLite
 
 module DataBaseConnection =
     
-    // let dbFile = "Data Source=ProjectAlphaDatabase.db"
-
-    let dbFile = "Data Source=ProjectAlphaDatabase.db;Version=3;Journal Mode=WAL;Pooling=True;Max Pool Size=100;"
+    let dbFile = "Data Source=ProjectAlphaDatabase.db"    
 
     let connectToDatabase () =     
         use connection = new SQLiteConnection($"Data Source={dbFile}")
